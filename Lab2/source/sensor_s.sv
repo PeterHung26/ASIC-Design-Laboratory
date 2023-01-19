af2be7482a6b9f4e7daa6e388d646bee41b5bbd3
Wen-Bo Hung <hong395@purdue.edu>
2023-01-18 23:07:29 -0500

    initial set up for Lab2

---
+++ Lab2/source/sensor_s.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_s.sv
+// Created:     1/18/2023
+// Author:      Wen-Bo Hung
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: Structural Style Sensor Error Detector.
